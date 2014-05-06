a074aecd53d01648a889f67cbb243755532af685
sleepwalking <2657202503@qq.com>
2014-05-06 18:46:42 +0800

    Precise F0 estimation.

--- src/_F0.h
+++ src/_F0.h
@@ -5,6 +5,9 @@ RTFunc(int, CSVP_F0FromSpectrumB, _Spectrum* Sorc, int MinFreq, int MaxFreq,
 RTFunc(_T1, CSVP_F0FromSpectrum, _Spectrum* Sorc, int MinFreq, int MaxFreq,
     int SampleRate);
 
+RTFunc(_T1, CSVP_F0FromSuccSpectrumAndBin, _Spectrum* Sorc1, _Spectrum* Sorc2,
+    int HopSize, int Bin, int SampleRate);
+
 RTFunc(_T1, CSVP_F0FromWave, void* Sorc, int Position, int WinSize,
     int MinFreq, int MaxFreq, int SampleRate);
 
--- test/TestF0/main.c
+++ test/TestF0/main.c
@@ -6,29 +6,32 @@ int main()
     CDSP2_SetArch(CDSP2_Arch_Gnrc);
     CDSP2_Wave_Float MyWave;
     CDSP2_Wave_Float_Ctor(& MyWave);
-    CDSP2_Spectrum_Float MySpectrum;
+    CDSP2_Spectrum_Float MySpectrum, MySpectrum2;
     CDSP2_Spectrum_Float_CtorSize(& MySpectrum, 2048);
+    CDSP2_Spectrum_Float_CtorSize(& MySpectrum2, 2048);
     
-    Double* wind = RAlloc_Double(2048);
-    CDSP2_VSet_Double(wind, 1, 2048);
-    CDSP2_Hanning_Double(wind, wind, 2048);
-    CDSP2_IWave_Double_SetWindow(& MyWave, wind, 2048);
+    Float* wind = RAlloc_Float(2048);
+    CDSP2_GenHanning_Float(wind, 2048);
+    CDSP2_Wave_Float_SetWindow(& MyWave, wind, 2048);
     
     //RNew(CDSP2_Wave_Float, & MyWave);
     String_FromChars(Path, "/tmp/test.wav");
     
     int ret = CDSP2_Wave_Float_FromFile(& MyWave, & Path);
-    printf("ret: %d\n", ret);
-    MySpectrum.MagnType = CDSP2_LogMagn;
-    CDSP2_Spectrum_Float_FromWave(& MySpectrum, & MyWave, 97000);
-    
     int i;
-    //for(i= 0; i < 300; i ++)
-    //    printf("%f\n", MySpectrum.Magn[i]);
-    int F0 = CSVP_F0FromSpectrumB_Float(& MySpectrum, 100, 1500, 44100);
-    printf("F0: %d\n", F0 * 44100 / 2048);
     
-    RDelete(& MyWave, & MySpectrum, & Path);
+    //printf("ret: %d\n", ret);
+    MySpectrum.MagnType = CDSP2_LogMagn;
+    MySpectrum2.MagnType = CDSP2_LogMagn;
+    CDSP2_Spectrum_Float_FromWaveW(& MySpectrum, & MyWave, 200000);
+    CDSP2_Spectrum_Float_FromWaveW(& MySpectrum2, & MyWave, 200002);
+    
+    //for(i= 0; i < 300; i ++) printf("%f\n", MySpectrum.Phse[i]);
+    int F0 = CSVP_F0FromSpectrumB_Float(& MySpectrum, 100, 1500, 48000);
+    int F0_2 = CSVP_F0FromSpectrumB_Float(& MySpectrum2, 100, 1500, 48000);
+    printf("F0: %d %f\n", F0, CSVP_F0FromSuccSpectrumAndBin_Float(& MySpectrum, & MySpectrum2, 2, F0, 48000));
+    
+    RDelete(& MyWave, & MySpectrum, & MySpectrum2, & Path);
     RFree(wind);
     return 0;
 }
