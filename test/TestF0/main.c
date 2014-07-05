3d477670cdb543d26f0ca31b35ebdd904a728e6a
sleepwalking <2657202503@qq.com>
2014-07-05 20:55:13 +0800

    Harmonic Comb doesn't work, removed.

--- test/TestF0/main.c
+++ test/TestF0/main.c
@@ -13,21 +13,21 @@ int main()
     CDSP2_GenHanning_Float(wind, 2048);
     CDSP2_Wave_Float_SetWindow(& MyWave, wind, 2048);
     
-    String_FromChars(Path, "/tmp/burn.wav");
+    String_FromChars(Path, "/tmp/test2.wav");
     
     CDSP2_Wave_Float_FromFile(& MyWave, & Path);
     
     CSVP_F0Iterlyzer_Float F0Iter;
     CSVP_F0Iterlyzer_Float_Ctor(& F0Iter);
     F0Iter.Option.Adlib = 1;
-    F0Iter.Option.LFreq = 100;
-    F0Iter.Option.HFreq = 1000;
+    F0Iter.Option.LFreq = 50;
+    F0Iter.Option.HFreq = 800;
     F0Iter.Option.Threshold = 0.01;
     
     int Onset = CSVP_OnsetFromWave_Float(& MyWave, 0.0005, 0, MyWave.Size);
     int VOT = CSVP_VOTFromWave_Float(& MyWave, Onset, Onset + 10000);
     
-    CSVP_F0Iterlyzer_Float_SetHopSize(& F0Iter, 256);
+    CSVP_F0Iterlyzer_Float_SetHopSize(& F0Iter, 128);
     CSVP_F0Iterlyzer_Float_SetWave(& F0Iter, & MyWave);
     CSVP_F0Iterlyzer_Float_SetPosition(& F0Iter, VOT + 2000);
     CSVP_F0Iterlyzer_Float_PreAnalysisTo(& F0Iter, VOT + 10000);
@@ -37,7 +37,7 @@ int main()
     CSVP_F0Iterlyzer_Float_SetPosition(& F0Iter, VOT + 2000);
     F0Iter.RefF0 = RefF0;
     CSVP_F0Iterlyzer_Float_IterNextTo(& F0Iter, MyWave.Size - 3000);
-    CSVP_F0PostProcess_Float(& F0Iter.F0List, 4000, 0.05);
+    CSVP_F0PostProcess_Float(& F0Iter.F0List, 4000, 0.15);
     
     int i;
     for(i = 0; i <= F0Iter.F0List.X_Index; i ++)
